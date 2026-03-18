9eb848ccad43402c4bbf3ff58a68465a2794592e
laracmv <cmvlara1@gmail.com>
2026-03-18 18:05:43 -0300

    botoes

--- main/main.c
+++ main/main.c
@@ -7,10 +7,86 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include "hardware/adc.h"
+
+const int BTN_PIN_RED = 15;
+const int BTN_PIN_BLUE = 14;
+const int BTN_PIN_GREEN = 12;
+const int BTN_PIN_YELLOW = 11;
+
+volatile bool seed_timer_fired = false;
+
+volatile int flag_red = 0;
+volatile int flag_blue = 0;
+volatile int flag_green = 0;
+volatile int flag_yellow = 0;
+
+int64_t seed_alarm_callback(alarm_id_t id, void *user_data){
+    seed_timer_fired = true;
+    return 0;
+}
+
+void btn_callback(uint gpio, uint32_t event_mask) {
+    
+    if (gpio == BTN_PIN_BLUE){
+        flag_blue = 1;
+    }
+    else if (gpio == BTN_PIN_GREEN){
+        flag_green = 1;
+    }
+    else if (gpio == BTN_PIN_YELLOW){
+        flag_yellow = 1;
+    }
+    else if (gpio == BTN_PIN_RED){
+        flag_red = 1;
+    }
+        
+    
+}
 
 int main() {
     stdio_init_all();
+
+    
+
+    gpio_init(BTN_PIN_RED);
+    gpio_set_dir(BTN_PIN_RED, GPIO_IN);
+    gpio_pull_up(BTN_PIN_RED);
+
+    gpio_init(BTN_PIN_BLUE);
+    gpio_set_dir(BTN_PIN_BLUE, GPIO_IN);
+    gpio_pull_up(BTN_PIN_BLUE);
+
+    gpio_init(BTN_PIN_GREEN);
+    gpio_set_dir(BTN_PIN_GREEN, GPIO_IN);
+    gpio_pull_up(BTN_PIN_GREEN);
+
+    gpio_init(BTN_PIN_YELLOW);
+    gpio_set_dir(BTN_PIN_YELLOW, GPIO_IN);
+    gpio_pull_up(BTN_PIN_YELLOW);
+
+    gpio_set_irq_enabled_with_callback(BTN_PIN_RED, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+    gpio_set_irq_enabled(BTN_PIN_BLUE, GPIO_IRQ_EDGE_FALL, true);
+
+    gpio_set_irq_enabled(BTN_PIN_GREEN, GPIO_IRQ_EDGE_FALL, true);
+
+    gpio_set_irq_enabled(BTN_PIN_YELLOW, GPIO_IRQ_EDGE_FALL, true);
+
+    uint32_t start_ms = to_ms_since_boot(get_absolute_time());
+
     while (true) {
+        
+
+        if (flag_red){
+            printf("Red button pressed!\n");
+            flag_red = 0;
+            uint32_t end_ms = to_ms_since_boot(get_absolute_time());
+            uint32_t seed_time = end_ms - start_ms;
+            printf("Seed time: %d ms\n", seed_time);
+        }
+
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
